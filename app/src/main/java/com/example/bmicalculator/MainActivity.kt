e854468ebffa5e237be032aec9a0940670a62f4f
icarpio <icarpiodeveloper@gmail.com>
2024-06-06 10:37:03 +0200

    Add activity

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,34 +1,80 @@
 package com.example.bmicalculator
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
-    lateinit var welcomeTextView:TextView
-    lateinit var welcomeButton:Button
-    lateinit var welcomeImage:ImageView
-    var loading:Boolean = false
+    lateinit var heightEditText:EditText
+    lateinit var weightEditText:TextView
+    lateinit var minusButton:Button
+    lateinit var addButton:Button
+    lateinit var resultTextView: TextView
+    lateinit var healthyTextView: TextView
+    lateinit var calculateButton:Button
+    var height:Float=0.0F
+    var weight:Float=60.0F
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        welcomeTextView = findViewById(R.id.welcomeTextView)
-        welcomeButton = findViewById(R.id.welcomeButton)
-        welcomeImage = findViewById(R.id.welcomeImageView)
+        heightEditText = findViewById(R.id.heightEditText)
+        weightEditText = findViewById(R.id.weightEditText)
+        minusButton =  findViewById(R.id.minusButton)
+        addButton  = findViewById(R.id.addButton)
+        healthyTextView = findViewById(R.id.healthyTextView)
+        resultTextView = findViewById(R.id.resultTextView)
+        calculateButton = findViewById(R.id.calculateButton)
 
-        welcomeButton.setOnClickListener {
-                loading = true
-                welcomeTextView.text = getString(R.string.main_activity_click_welcome)
-                //cambia la imagen:
-                // welcomeImage.setImageResource(R.drawable.image2)
-                //Imagen transparente
-                welcomeImage.setImageResource(android.R.color.transparent)
-                println(loading)
+        //Controles del peso
+        minusButton.setOnClickListener {
+            weight --
+            setWeight()
+            Log.i("IMCS","Reducir peso")
+        }
+
+        addButton.setOnClickListener {
+            weight ++
+            setWeight()
+            Log.i("IMCS","Aumentar peso")
+        }
+        //Calcular resultado
+        calculateButton.setOnClickListener {
+            height = heightEditText.text.toString().toFloat()
+            val result = weight / (height / 100f).pow(2)
+            val formated = String.format("%.2f", result)
+            resultTextView.text = formated
+            when (result) {
+                in 0.0 ..18.5 -> {
+                    healthyTextView.text = "Bajo peso"
+
+                }
+                in 18.5..24.9 -> healthyTextView.text = "Peso Normal"
+                in 25.0..29.9 -> healthyTextView.text = "Sobrepeso"
+                else -> {
+                    healthyTextView.text = "Obesidad"
+                    healthyTextView.setTextColor(Color.parseColor("#EF330A"))
+                }
+            }
         }
     }
+    fun setHeight() {
+        heightEditText.setText(height.toString())
+    }
+
+    fun setWeight() {
+        weightEditText.text = "$weight Kg"
+    }
+
 
 }
