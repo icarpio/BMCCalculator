8928f911911bbf1497d3dbb75bc230f7210821ca
icarpio <icarpiodeveloper@gmail.com>
2024-06-07 09:44:27 +0200

    Add slider

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -10,10 +10,11 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import com.google.android.material.slider.Slider
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
-    lateinit var heightEditText: EditText
+
     lateinit var weightEditText: TextView
     lateinit var minusButton: Button
     lateinit var addButton: Button
@@ -21,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var healthyTextView: TextView
     lateinit var calculateButton: Button
     lateinit var imageView:ImageView
+    lateinit var heightSlider:Slider
 
     var height: Float = 0.0F
     var weight: Float = 60.0F
@@ -30,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        heightEditText = findViewById(R.id.heightEditText)
+        heightSlider = findViewById(R.id.heightSlider)
         weightEditText = findViewById(R.id.weightEditText)
         minusButton = findViewById(R.id.minusButton)
         addButton = findViewById(R.id.addButton)
@@ -45,20 +47,24 @@ class MainActivity : AppCompatActivity() {
         minusButton.setOnClickListener {
             weight--
             setWeight()
-            Log.i("IMCS", "Reducir peso")
         }
 
         addButton.setOnClickListener {
             weight++
             setWeight()
-            Log.i("IMCS", "Aumentar peso")
+        }
+        heightSlider.addOnChangeListener { _, value, _ ->
+            height = value
+            height.toString()
+            Log.i("Slider", height.toString())
         }
         //Calcular resultado
         calculateButton.setOnClickListener {
-            height = heightEditText.text.toString().toFloat()
             val result = weight / (height / 100f).pow(2)
             val formated = String.format("%.2f", result)
             resultTextView.text = formated
+
+
             when (result) {
                 in 0.0..18.5 -> {
                     healthyTextView.text = "Bajo peso"
@@ -88,9 +94,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun setHeight() {
-        heightEditText.setText(height.toString())
-    }
 
     fun setWeight() {
         weightEditText.text = "$weight Kg"
