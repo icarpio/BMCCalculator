0877fd7f66ed7a89c34edadf26a23176c4f2860a
icarpio <icarpiodeveloper@gmail.com>
2024-06-10 10:14:28 +0200

    Add title in toolbar

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -14,7 +14,7 @@ import com.google.android.material.slider.Slider
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var heightEditText: TextView
     lateinit var weightEditText: TextView
     lateinit var minusButton: Button
     lateinit var addButton: Button
@@ -32,6 +32,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.title = "BMI Calculator"
+
+        heightEditText = findViewById(R.id.heightEditText)
         heightSlider = findViewById(R.id.heightSlider)
         weightEditText = findViewById(R.id.weightEditText)
         minusButton = findViewById(R.id.minusButton)
@@ -55,8 +58,8 @@ class MainActivity : AppCompatActivity() {
         }
         heightSlider.addOnChangeListener { _, value, _ ->
             height = value
-            height.toString()
-            Log.i("Slider", height.toString())
+            setHeight()
+            Log.i("Slider", height.toInt().toString())
         }
         //Calcular resultado
         calculateButton.setOnClickListener {
@@ -64,39 +67,46 @@ class MainActivity : AppCompatActivity() {
             val formated = String.format("%.2f", result)
             resultTextView.text = formated
 
+            var color = 0
 
             when (result) {
                 in 0.0..18.5 -> {
-                    healthyTextView.text = "Bajo peso"
+                    healthyTextView.text = getString(R.string.under_weight)
                     healthyTextView.setTextColor(Color.parseColor("#FF9933"))
-                    imageView.visibility = View.INVISIBLE
+                    imageView.visibility = View.GONE
 
                 }
 
                 in 18.5..24.9 -> {
-                    healthyTextView.text = "Peso Normal"
+                    healthyTextView.text = getString(R.string.normal)
                     healthyTextView.setTextColor(Color.parseColor("#33FF36"))
-                    imageView.visibility = View.INVISIBLE
+                    imageView.visibility = View.GONE
                 }
 
                 in 25.0..29.9 -> {
-                    healthyTextView.text = "Sobrepeso"
+                    healthyTextView.text = getString(R.string.overweight)
                     healthyTextView.setTextColor(Color.parseColor("#33FF36"))
-                    imageView.visibility = View.INVISIBLE
+                    imageView.visibility = View.GONE
                 }
 
                 else -> {
-                    healthyTextView.text = "Obesidad"
-                    healthyTextView.setTextColor(Color.parseColor("#F33F0A"))
+                    healthyTextView.text = getString(R.string.obesity)
+                    color = getColor(R.color.weight_red)
+                    healthyTextView.setTextColor(color)
                     imageView.visibility = View.VISIBLE
                 }
             }
         }
     }
 
-
     fun setWeight() {
-        weightEditText.text = "$weight Kg"
+        //weightEditText.text = "$weight Kg"
+        weightEditText.text = getString(R.string.weight_text, weight.toInt())
+    }
+    fun setHeight() {
+        var formated = height.toInt().toString()
+        heightEditText.text = "$formated cm"
+        //heightEditText.text = getString(R.string.height_text, height)
     }
 
 
